932a975122291e5218e380e7ac57c34bc1f5240e
balbir.singh <balbir.singh@ezeiatech.in>
2020-11-04 11:58:02 +0530

    implementation of JWT

--- src/main/java/spring/security/demo/security/ApplicationSecurityCofig.java
+++ src/main/java/spring/security/demo/security/ApplicationSecurityCofig.java
@@ -1,22 +1,21 @@
 package spring.security.demo.security;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import spring.security.demo.auth.ApplicationUserService;
+import spring.security.demo.jwt.JwtTokenVerifierFilter;
+import spring.security.demo.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -36,36 +35,15 @@ public class ApplicationSecurityCofig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.csrf().disable()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
+			.addFilterAfter(new JwtTokenVerifierFilter(), JwtUsernameAndPasswordAuthenticationFilter.class)
 			.authorizeRequests()
 			.antMatchers("/","index","/css/*","/js/*").permitAll()
-//			.antMatchers("/student/*").hasRole(STUDENT.name())
-//			.antMatchers(HttpMethod.POST,"/management/*").hasAuthority(STUDENT_WRITE.getPermission())
-//			.antMatchers(HttpMethod.PUT,"/management/*").hasAuthority(STUDENT_WRITE.getPermission())
-//			.antMatchers(HttpMethod.DELETE,"/management/*").hasAuthority(STUDENT_WRITE.getPermission())
-//			.antMatchers(HttpMethod.GET,"/management/*").hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
 			.anyRequest()
-			.authenticated()
-			.and()
-//			.httpBasic();
-			.formLogin()
-				.loginPage("/login").permitAll()
-				.defaultSuccessUrl("/courses",true)
-				.usernameParameter("username")			// default username
-				.passwordParameter("password")			// default password
-			.and()
-			.rememberMe()  // default 2 weeks
-				.tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-				.key("sometingVerySecured")
-				.userDetailsService(userDetailsService)
-				.rememberMeParameter("remember-me") 	// default remember-me
-			.and()
-			.logout()
-				.logoutUrl("/logout")
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", HttpMethod.GET.name()))
-				.clearAuthentication(true)
-				.invalidateHttpSession(true)
-				.deleteCookies("JSESSIONID","remember-me")
-				.logoutSuccessUrl("/login");
+			.authenticated();
 		
 	}
 	
